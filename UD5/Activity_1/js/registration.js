b39cf4291762e1050f6ed73e35fb8e6d086e8bf3
daviddrm52 <davidgranada2015@gmail.com>
2023-11-24 17:13:21 +0100

    it loads data from the form, clean overtake

--- UD5/Activity_1/js/registration.js
+++ UD5/Activity_1/js/registration.js
@@ -1,74 +1,109 @@
-window.onload = function() {
-    openDatabase();
-};
+var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+var database = "usersDB";
+const DB_STORE_NAME = 'users';
+const DB_VERSION = 1;
+var db
+var opened = false;
+var sendData = document.querySelector("#sendDataForm");
 
-function openDatabase() {
+function openCreateDatabase(onDBCompleted) {
+    if(opened){
+        db.close();
+        opened = false;
+    }
+    
     // Opening the database
-    const request = window.indexedDB.open("usersDB", 1);
-    let db;
-
-    //onError handler
-    request.onerror = (event) => {
-        console.error("The web browser doesn't want to use indexedDB, this hits hard");
-    };
+    var request = indexedDB.open(database, DB_VERSION);
 
     //onsuccess handler (works)
     request.onsuccess = (event) => {
         db = event.target.result;
-        console.log("database opened");
+
+        console.log(db);
+        console.log("database opened " + db);
+        opened = true;
+        onDBCompleted(db);
     };
 
     //onupgradeneeded handler (this is for the first time the db is created)(works)
-    request.onupgradeneeded = (event) => {
-        const db = event.target.result;
-        const objectStore = db.createObjectStore("users", {keyPath: "id", autoIncrement: true});
+    request.onupgradeneeded = () => {
+        db = request.result;
+        console.log("openCreateDatabase: upgrade is needed "+ db);
+        var objectStore = db.createObjectStore(DB_STORE_NAME, {keyPath: "id", autoIncrement: true});
+        console.log("openCreateDatabase: Objct store created succesfully");
+        
         objectStore.createIndex("name", "name", {unique: false});
+        console.log("openCreateDatabase: Index created on name");
         objectStore.createIndex("username", "username", {unique: true});
+        console.log("openCreateDatabase: Index created on username");
         objectStore.createIndex("email", "email", {unique: true});
+        console.log("openCreateDatabase: Index created on email");
         objectStore.createIndex("password", "password", {unique: false});
-        console.log("Index created: name, username, email, password");
+        console.log("openCreateDatabase: Index created on password");
+    };
 
-        //Data for the example
-        const userData = [
-            {name: "Crewmate", username: "Crewmate_good_18", email: "crewmate_amongus@gmail.com", password: "12345678"},
-            {name: "Impostor", username: "Impostor_bad_43", email: "impostor_amongus@gmail.com", password: "87654321"},
-        ];
-        //oncomplete to finish the operation, and to add data
-        objectStore.transaction.oncomplete = (event) => {
-            const userObjectStore = db.transaction("users", "readwrite").objectStore("users");
-            userData.forEach((user) => {
-                userObjectStore.add(user);
-            });
-        };
+    //onError handler
+    request.onerror = (event) => {
+        console.error("Error opening or creating a database, this error hits hard: ", event.target.errorCode);
+    };
+};
+  
+//sendDataForm
+function sendDataForm(){
+    openCreateDatabase(testing);
+};
+
+function testing(db){
+    var hiddenId = document.getElementById("hiddenId").value;
+    if(hiddenId == 0){
+        addUserForm(db);
     };
 };
 
-function addUserFromForm(db) {
+//addUserForm
+function addUserForm(db) {
     var name = document.getElementById("name");
     var username = document.getElementById("username");
     var email = document.getElementById("email");
     var password = document.getElementById("password");
+
     var object = {name: name.value, username: username.value, email: email.value, password: password.value};
 
-    //Start a new transaction, this is perhaps pepega
-    var tx = db.transaction("users", "readwrite");
-    var store = tx.objectStore("users");
+    //Start transaction
+    var tx = db.transaction(DB_STORE_NAME, "readwrite");
+    var store = tx.objectStore(DB_STORE_NAME);
 
+    //tryCatch
     try {
-        store.add(object);
+        request = store.add(object);
     } catch (e) {
         console.log("Catch");
     };
 
     request.onsuccess = (event) => {
-        console.log("addUserFromForm: Data insertion successfully done. Id: "+event.target.result);
+        console.log("addUserForm: Data insertion successfully done. ID: " + event.target.result);
+
+        //Operations we want to do after inserting data
+        /* Clean inputs from the form */
+
     };
 
     request.onerror = (event) => {
-        console.log("addUserFromForm: error creating data", this.error);
+        console.error("addUserForm: error creating data!", this.error);
     };
 
-    request.oncomplete = (event) => {
-        console.log("transaction completed");
-    }
+    //After transaction is completed, we close the database
+    tx.oncomplete = () => {
+        console.log("addUserForm: transaction completed!");
+        db.close();
+        opened = false;
+    };
 };
+
+
+//eventListener for when the form is send
+window.addEventListener('load', (event) =>{
+    sendData.addEventListener('click', (event) => {
+        sendDataForm();
+    });
+});
