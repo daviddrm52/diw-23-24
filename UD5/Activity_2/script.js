b21eb9b172d419f5579a83c599126a4033c918aa
daviddrm52 <davidgranada2015@gmail.com>
2023-12-13 14:39:31 +0100

    Activity 2 finished, starting activity 3

--- UD5/Activity_2/script.js
+++ UD5/Activity_2/script.js
@@ -14,11 +14,11 @@ var sales = [
     }
 ];
 
-//Variables
+//Variables for the canvas
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-//For the text
+//To print the text into the canvas
 ctx.font = "bold 15px Arial";
 ctx.fillText("Units", 25, 250);
 ctx.fillText("Product", 300, 470);
@@ -26,7 +26,7 @@ ctx.fillText(sales[0].product, 150, 420);
 ctx.fillText(sales[1].product, 300, 420);
 ctx.fillText(sales[2].product, 450, 420);
 
-//For the basketball
+//Gradient for the basketballs
 const basketball = ctx.createLinearGradient(150, 0, 225, 0);
 basketball.addColorStop(0, "#fea708");
 basketball.addColorStop(1, "#efe2df");
@@ -35,12 +35,25 @@ basketball.addColorStop(1, "#efe2df");
 ctx.fillStyle = basketball;
 ctx.fillRect(150, 400, 75, -sales[0].units);
 
-//For the baseballs
+//Gradient for the baseballs
+const baseball = ctx.createLinearGradient(300, 0, 375, 0);
+baseball.addColorStop(0, "#0954c0");
+baseball.addColorStop(1, "#efe2df");
 
+// Fill with gradient for the baseballs
+ctx.fillStyle = baseball;
+ctx.fillRect(300, 400, 75, -sales[1].units);
 
-//For the footballs
+//Gradient for the footballs
+const football = ctx.createLinearGradient(450, 0, 525, 0);
+football.addColorStop(0, "#f17944");
+football.addColorStop(1, "#efe2df");
 
+// Fill with gradient for the footballs
+ctx.fillStyle = football;
+ctx.fillRect(450, 400, 75, -sales[2].units);
 
+//For the line in the graph
 ctx.beginPath();
 //Top arrow
 ctx.moveTo(100,5);
@@ -59,5 +72,3 @@ ctx.lineTo(590,405);
 ctx.moveTo(595,400);
 ctx.lineTo(590,395);
 ctx.stroke();
-
-
