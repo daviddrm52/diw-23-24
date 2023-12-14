4cf8f235cbcb70b84c933915080f4e0ff8f50411
daviddrm52 <davidgranada2015@gmail.com>
2023-12-14 21:12:00 +0100

    finished with image and new sound

--- UD5/Activity_3/script.js
+++ UD5/Activity_3/script.js
@@ -1,6 +1,8 @@
+//Variables
 let canvas;
 let context;
 let sound = document.querySelector("#boing");
+//The change of color is funny
 let color = ["red", "yellow", "green", "blue", "orange", "purple"];
 let randomColor;
 let dingus;
@@ -9,44 +11,35 @@ let stopButton = document.getElementById("stopPingPong");
 let statusGame = document.getElementById("statusGame");
 let axisX;
 let axisY;
+let axisXInput = document.getElementById("axis-X");
+let axisYInput = document.getElementById("axis-Y");
+var ballX = 10;
+var ballY = 10;
+var directionX;
+var directionY;
 
-startButton.addEventListener('click', (event) => {
-    axisX = document.getElementById("axis-X").value;
-    axisY = document.getElementById("axis-Y").value;
-    directionX = parseInt(axisX);
-    directionY = parseInt(axisY);
-    statusGame.innerHTML = "Active";
-    dingus = setInterval(movement, 10);
-    // alert("⚠ WARNING: The ball has started! ⚠")
-});
+//For the image
+var image = new Image();
+image.src = './img/kaisa.jpg';
 
-stopButton.addEventListener('click', (event) => {
-    clearInterval(dingus);
-    statusGame.innerHTML = "Inactive";
-    // alert("⚠ WARNING: The ball has stoped! ⚠")
-});
-
+//Starting the canvas
 canvas = document.getElementById('2d-animation-canvas');
 context = canvas.getContext('2d');
+stopButton.disabled = true;
 
+//Functions
 function draw(x, y) {
-    context.fillStyle = randomColor;
-    context.beginPath();
-    context.arc(x,y,10,0,Math.PI * 2,true);
-    context.fill();
+    // context.fillStyle = "#800080";
+    // context.beginPath();
+    // context.arc(x,y,10,0,Math.PI * 2,true);
+    // context.fill();
+    context.drawImage(image,x,y, 45,45);
 };
 
 function clearCanvas() {
     canvas.width = canvas.width;
 };
 
-var ballX = 10;
-var ballY = 10;
-var directionX;
-var directionY;
-
-draw(ballX, ballY);
-    
 function movement(){
     if (ballX > 800 || ballX < 0){
         directionX *= -1;
@@ -62,7 +55,37 @@ function movement(){
     ballY += directionY;
     clearCanvas();
     draw(ballX, ballY);
-
-    console.log(directionX);
-    console.log(directionY);
 };
+
+draw(ballX, ballY);
+
+//Event Listeners
+axisXInput.addEventListener('click', (event) => {
+    axisX = document.getElementById("axis-X").value;
+    directionX = parseInt(axisX);
+});
+
+axisYInput.addEventListener('click', (event) => {
+    axisY = document.getElementById("axis-Y").value;
+    directionY = parseInt(axisY);
+})
+
+startButton.addEventListener('click', (event) => {
+    axisX = document.getElementById("axis-X").value;
+    axisY = document.getElementById("axis-Y").value;
+    directionX = parseInt(axisX);
+    directionY = parseInt(axisY);
+    statusGame.innerHTML = "Active";
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    dingus = setInterval(movement, 15);
+    console.log("Ball active");
+});
+
+stopButton.addEventListener('click', (event) => {
+    clearInterval(dingus);
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    statusGame.innerHTML = "Inactive";
+    console.log("Ball inactive");
+});
