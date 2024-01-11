aa4a645cdee2549ac5fbab426975dac066cd2e1e
daviddrm52 <davidgranada2015@gmail.com>
2024-01-11 21:16:54 +0100

    working on it, need research on how jquery works sometimes

--- UD6/Activity_2/index.js
+++ UD6/Activity_2/index.js
@@ -1,16 +1,21 @@
 $(document).ready (function(){
     console.log("Document is ready");
 
+    let kiryu; //Kiryu best driver from Yakuza 5 kek
+
     for(i = 0; i < 20; i++){
-        $("#board").append("<div class='card' id='card-"+(i+1)+"'><h1>"+(i+1)+"</h1></div>");
+        kiryu = Math.floor((Math.random() * 10) + 1);
+        $(".board").append("<div class='card' value='"+kiryu+"' id='card-"+kiryu+"'></div>");
     };
 
     $(".card").on("click", function() {
-        var id = $(this).attr('id');
-        console.log(id);
+        var cardValue = $(this).attr("value");
+        $(this).text(cardValue);
         $(this).toggleClass("inactive");
+        $("#message").text("上次检查的卡号: "+ cardValue);
         setTimeout(() => {
             $(this).toggleClass("inactive");
+            $(this).text("");
         }, 1000);
     });
-})
+});
