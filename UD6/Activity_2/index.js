a17092ca7248578b2f58086d6f79f31b5eb4cb19
daviddrm52 <davidgranada2015@gmail.com>
2024-01-12 17:17:24 +0100

    base game fully working, in japanese for the memes kek

--- UD6/Activity_2/index.js
+++ UD6/Activity_2/index.js
@@ -1,21 +1,114 @@
+//General variables
+var kiryuCounter = 0;
+
+// JQuery things
 $(document).ready (function(){
-    console.log("Document is ready");
+    console.log("Activity 2 - UD6 - David Rueda Madrid is ready");
 
-    let kiryu; //Kiryu best driver from Yakuza 5 kek
+    //Variables
+    var firstPairSelected;
+    var secondPairSelected;
+    var firstCardID;
+    var secondCardID;
+    //To randomize the array with the numbers
+    var firstPair = randomCardNumber([1,2,3,4,5,6,7,8,9,10]);
+    var secondPair = randomCardNumber([1,2,3,4,5,6,7,8,9,10]);
 
-    for(i = 0; i < 20; i++){
-        kiryu = Math.floor((Math.random() * 10) + 1);
-        $(".board").append("<div class='card' value='"+kiryu+"' id='card-"+kiryu+"'></div>");
-    };
+    //To print the randomized array into the page
+    for(i = 0; i < firstPair.length; i++){
+        $(".board").append("<div class='card-first' value='"+firstPair[i]+"' id='card-first-"+firstPair[i]+"'>"+firstPair[i]+"</div>");
+    }
+    for(i = 0; i < secondPair.length; i++){
+        $(".board").append("<div class='card-second' value='"+secondPair[i]+"' id='card-second-"+secondPair[i]+"'>"+secondPair[i]+"</div>");
+    }
 
-    $(".card").on("click", function() {
-        var cardValue = $(this).attr("value");
-        $(this).text(cardValue);
-        $(this).toggleClass("inactive");
-        $("#message").text("上次检查的卡号: "+ cardValue);
-        setTimeout(() => {
+    //When the user clicks in the top set of cards
+    $(".card-first").on("click", function() {
+        if(!$(this).hasClass("pair-correct")){
+            kiryuCounter++;
+            var cardValue = $(this).attr("value");
+            firstCardID = $(this).attr("id");
+            console.log(firstCardID);
+            firstPairSelected = cardValue;
             $(this).toggleClass("inactive");
-            $(this).text("");
-        }, 1000);
+            $("#message-first-card").text("最初のカードからの番号： "+ cardValue);
+            checkCard(cardValue);
+            if(firstPairSelected != null && secondPairSelected != null){
+                checkPair(firstPairSelected, firstCardID, secondPairSelected, secondCardID);
+            };
+            setTimeout(() => {
+                if(!$(this).hasClass("pair-correct")) {
+                    $(this).toggleClass("inactive"); 
+                }
+                firstPairSelected = null;
+            }, 750);
+        }
+    });
+    
+    //When the user clicks in the bottom set of cards
+    $(".card-second").on("click", function() {
+        if(!$(this).hasClass("pair-correct")){
+            kiryuCounter++;
+            var cardValue = $(this).attr("value");
+            secondPairSelected = cardValue;
+            secondCardID = $(this).attr("id");
+            $(this).toggleClass("inactive");
+            $("#message-second-card").text("枚目のカードの番号："+ cardValue);
+            checkCard(cardValue);
+            if(firstPairSelected != null && secondPairSelected != null){
+                checkPair(firstPairSelected, firstCardID, secondPairSelected, secondCardID);
+            };
+            setTimeout(() => {
+                if(!$(this).hasClass("pair-correct")) {
+                    $(this).toggleClass("inactive"); 
+                }
+                secondPairSelected = null;
+            }, 750);
+        }
     });
 });
+
+// FUNCTIONS
+
+//Function to test the value of the card
+function checkCard(card) {
+    console.log("Value of the card: "+card);
+    $("#kiryu").text("あなたは" +kiryuCounter+" 枚のカードをチェックしました");
+}
+
+//Function to randomize the cards (it works actually)
+function randomCardNumber(array){
+    var i = array.length, j = 0, temp;
+    while(i--){
+        j = Math.floor(Math.random() * (i+1));
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    };
+    return array;
+};
+
+//Function to check if the top & bot set of selected cards pairs
+function checkPair(first, idFirst, second, idSecond) {
+    if (first === second){
+        $("#message-pairing").text("2カードマッチ.");
+        $("div#"+idFirst).addClass("pair-correct");
+        $("div#"+idSecond).addClass("pair-correct");
+    } else {
+        $("#message-pairing").text("2枚のカードが一致しない.");
+    };
+};
+
+//TODO
+/*
+    Waiting to put extras to the game
+*/
+
+/**
+ * Extra notes
+ * 
+ * The game is in japanese, it is NOT recomended to translate with Google in the same
+ * webpage, will cause the numbers to change in the screen.
+ * 
+ * Why is in other language: i don't know, this is to verify that is made by daviddrm52
+ */
