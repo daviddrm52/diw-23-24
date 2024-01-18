c0002b3b26da8523014134afab3e71019da6982f
daviddrm52 <davidgranada2015@gmail.com>
2024-01-18 21:15:44 +0100

    activity 3 created & finished (i think)

---
+++ UD6/Activity_3/index.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    console.log("Document is ready");
+    //First solution
+    $("textArea").countCharacters();
+    //Second solution
+    // $("textArea").countCharacters();
+});
+
+// $.fn.countCharacters = function (){
+//     //For the first text area
+//     var textVal = $(this).val().length;
+//     $(this).after("<h4><span></span></h4>");
+//     //We use data() to store the variable that stores the length of the value inside the text area
+//     $("form").data("test", textVal);
+//     $("span").html($("form").data("test")+" characters");
+//     //This is to update the span when the user writes inside the text area
+//     $(this).keyup(function () {
+//         textVal = $(this).val().length;
+//         $("form").data("test", textVal);
+//         $("span").html($("form").data("test")+" characters");
+//     });
+// };
+
+$.fn.countCharacters = function (){
+    //For the first text area
+    var A346 = $("#firstTextArea").val().length;
+    //This will add the span 
+    $("#firstTextArea").after("<h4><span id='spanair'></span></h4>");
+    //We use data() to store the variable that stores the length of the value inside the text area
+    $("form").data("test", A346);
+    $("#spanair").html($("form").data("test")+" characters");
+    //This is to update the span when the user writes inside the text area
+    $("#firstTextArea").keyup(function () {
+        A346 = $("#firstTextArea").val().length;
+        $("form").data("test", A346);
+        $("#spanair").html($("form").data("test")+" characters");
+    });
+    //For the second text area
+    var B77L = $("#secondTextArea").val().length;
+    $("#secondTextArea").after("<h4><span id='spantax'></span></h4>");
+    //We use data() to store the variable that stores the length of the value inside the text area
+    $("form").data("test", B77L);
+    $("#spantax").html($("form").data("test")+" characters");
+    //This is to update the span when the user writes inside the text area
+    $("#secondTextArea").keyup(function() {
+        B77L = $("#secondTextArea").val().length;
+        $("form").data("test", B77L);
+        $("#spantax").html($("form").data("test")+" characters");
+    });
+};
+
+/* Notes:
+ * 
+ * A346 is the ICAO code for the Airbus A340-600
+ * B77L is the ICAO code for the Boeing 777-200LR & the Boeing 777F
+ */
