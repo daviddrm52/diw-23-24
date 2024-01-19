3f9cfe385a18854e45cbb2c63d1e4ed2b1fb20cd
daviddrm52 <davidgranada2015@gmail.com>
2024-01-19 14:45:20 +0100

    activity 3 revised & finished

--- UD6/Activity_3/index.js
+++ UD6/Activity_3/index.js
@@ -1,30 +1,15 @@
 $(document).ready(function () {
     console.log("Document is ready");
-    //First solution
     $("textArea").countCharacters();
-    //Second solution
-    // $("textArea").countCharacters();
+    //This work around doesn't work
+    // $("#firstTextArea").countCharacters();
+    // $("#secondTextArea").countCharacters();
 });
-
-// $.fn.countCharacters = function (){
-//     //For the first text area
-//     var textVal = $(this).val().length;
-//     $(this).after("<h4><span></span></h4>");
-//     //We use data() to store the variable that stores the length of the value inside the text area
-//     $("form").data("test", textVal);
-//     $("span").html($("form").data("test")+" characters");
-//     //This is to update the span when the user writes inside the text area
-//     $(this).keyup(function () {
-//         textVal = $(this).val().length;
-//         $("form").data("test", textVal);
-//         $("span").html($("form").data("test")+" characters");
-//     });
-// };
 
 $.fn.countCharacters = function (){
     //For the first text area
     var A346 = $("#firstTextArea").val().length;
-    //This will add the span 
+    //This will add the span after the text area
     $("#firstTextArea").after("<h4><span id='spanair'></span></h4>");
     //We use data() to store the variable that stores the length of the value inside the text area
     $("form").data("test", A346);
@@ -37,6 +22,7 @@ $.fn.countCharacters = function (){
     });
     //For the second text area
     var B77L = $("#secondTextArea").val().length;
+    //This will add the span after the text area
     $("#secondTextArea").after("<h4><span id='spantax'></span></h4>");
     //We use data() to store the variable that stores the length of the value inside the text area
     $("form").data("test", B77L);
@@ -54,3 +40,19 @@ $.fn.countCharacters = function (){
  * A346 is the ICAO code for the Airbus A340-600
  * B77L is the ICAO code for the Boeing 777-200LR & the Boeing 777F
  */
+
+// Base plugin (it works with one text area, but with two, it's not working)
+// $.fn.countCharacters = function (){
+//     //For the first text area
+//     var textVal = $(this).val().length;
+//     $(this).after("<h4><span></span></h4>");
+//     //We use data() to store the variable that stores the length of the value inside the text area
+//     $("form").data("test", textVal);
+//     $("span").html($("form").data("test")+" characters");
+//     //This is to update the span when the user writes inside the text area
+//     $(this).keyup(function () {
+//         textVal = $(this).val().length;
+//         $("form").data("test", textVal);
+//         $("span").html($("form").data("test")+" characters");
+//     });
+// };
