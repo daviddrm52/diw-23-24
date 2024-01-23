a3cb0a166755c9924fa144c693c33d3b5e5e83cf
daviddrm52 <davidgranada2015@gmail.com>
2024-01-23 19:38:22 +0100

    working on it

--- UD6/Activity_4/index.js
+++ UD6/Activity_4/index.js
@@ -1,16 +1,40 @@
-$( function() {
-    $( "#post-it-orange, #post-it-purple" ).draggable();
+var orangeCounter = 0;
+var purpleCounter = 0;
+
+$(document).ready( function() {
+    $( ".post-it-orange, .post-it-purple" ).draggable();
     $( "#orange-droppable" ).droppable({
-      accept: "#post-it-orange",
+      accept: ".post-it-orange",
       classes: {
         "ui-droppable-active": "ui-state-active",
         "ui-droppable-hover": "ui-state-hover"
       },
       drop: function( event, ui ) {
+        orangeCounter++;
         $( this )
           .addClass( "ui-state-highlight" )
           .find( "p" )
-            .html( "Air Chiquin has been detected" );
+          .html( "There are "+orangeCounter+" orange posts." );
       }
     });
-  } );
+    $( "#purple-droppable" ).droppable({
+      accept: ".post-it-purple",
+      classes: {
+        "ui-droppable-active": "ui-state-active",
+        "ui-droppable-hover": "ui-state-hover"
+      },
+      drop: function( event, ui ) {
+        purpleCounter++;
+        $( this )
+          .addClass( "ui-state-highlight" )
+          .find( "p" )
+            .html( "There are "+purpleCounter+" purple posts." );
+      }
+    });
+});
+
+$("#post-it-generator").on("click", function() {
+  test = "<div class='post-it-orange'><p></p></div>"
+  $("main").append(test);
+  $(".post-it-orange").draggable();
+});
