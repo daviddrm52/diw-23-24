9195303082b473817b0ba4cecc7fa8f041601c3a
daviddrm52 <davidgranada2015@gmail.com>
2024-01-29 22:57:07 +0100

    activity 4 finished, now is a good post-it

--- UD6/Activity_4/index.js
+++ UD6/Activity_4/index.js
@@ -10,19 +10,23 @@ $(document).ready( function() {
       accept: ".post-it-orange",
       drop: function(event, ui) {
         orangeIdentificator = ui.draggable.prop("id");
+        orangeDeleteButton = $("div#"+orangeIdentificator+" > p > button.delete-post-it-button")
         if($("div#"+orangeIdentificator).hasClass("orange-counted")){
           console.log("This post was in the droppable...");
         } else {
           orangeCounter++;
           $("div#"+orangeIdentificator).addClass("orange-counted");
+          orangeDeleteButton.prop("disabled", true);
           $(this).find("p").html("There are "+orangeCounter+" orange posts.");
         }
       },
       out: function( event, ui ) {
         orangeIdentificator = ui.draggable.prop("id");
+        orangeDeleteButton = $("div#"+orangeIdentificator+" > p > button.delete-post-it-button")
         if($("div#"+orangeIdentificator).hasClass("orange-counted")){
           orangeCounter--;
           $("div#"+orangeIdentificator).removeClass("orange-counted");
+          orangeDeleteButton.prop("disabled", false);
           $(this).find("p").html("There are "+orangeCounter+" orange posts.");
         }
       }
@@ -31,19 +35,23 @@ $(document).ready( function() {
       accept: ".post-it-purple",
       drop: function(event, ui) {
         purpleIdentificator = ui.draggable.prop("id");
+        purpleDeleteButton = $("div#"+purpleIdentificator+" > p > button.delete-post-it-button")
         if($("div#"+purpleIdentificator).hasClass("purple-counted")){
           console.log("This post was in the droppable...");
         } else {
           purpleCounter++;
           $("div#"+purpleIdentificator).addClass("purple-counted");
-          $(this).find( "p" ).html( "There are "+purpleCounter+" purple posts." );
+          purpleDeleteButton.prop("disabled", true);
+          $(this).find("p").html( "There are "+purpleCounter+" purple posts." );
         }
       },
       out: function(event, ui) {
         purpleIdentificator = ui.draggable.prop("id");
+        purpleDeleteButton = $("div#"+purpleIdentificator+" > p > button.delete-post-it-button")
         if($("div#"+purpleIdentificator).hasClass("purple-counted")){
           purpleCounter--;
           $("div#"+purpleIdentificator).removeClass("purple-counted");
+          purpleDeleteButton.prop("disabled", false);
           $(this).find("p").html("There are "+purpleCounter+" purple posts.");
         }
       }
@@ -51,10 +59,19 @@ $(document).ready( function() {
 
     $(document).on("click", ".delete-post-it-button", function(){
       postIiIdentificator = $(this).parent().parent().attr("id");
-      console.log(postIiIdentificator);
       $("#delete-post-it-dialog").dialog("open");
     });
     
+    $(document).on("click", ".minimize-post-it", function(){
+      postIiIdentificator = $(this).parent().parent().attr("id");
+      $("div#"+postIiIdentificator).addClass("minimized");
+    });
+
+    $(document).on("click", ".maximize-post-it", function(){
+      postIiIdentificator = $(this).parent().parent().attr("id");
+      $("div#"+postIiIdentificator).removeClass("minimized");
+    });
+
     $("#delete-post-it-dialog").dialog({
       autoOpen: false,
       resizable: false,
@@ -86,11 +103,11 @@ $(document).ready( function() {
       randomPostIt = Math.floor((Math.random() * 2) + 1);
       if(randomPostIt === 1) {
         orangeID++;
-        $("main").append("<div class='post-it-orange' id='orange-'"+orangeID+"><p><button class='minimize-post-it'>_</button> <button class='maximize-post-it'>※</button> <button class='delete-post-it-button'>X</button></p><textarea></textarea></div>");
+        $("main").append("<div class='post-it-orange' id='orange-'"+orangeID+"><p><button class='minimize-post-it'>_</button> <button class='maximize-post-it'>※</button> <button class='delete-post-it-button'>X</button></p><textarea maxlength='140'></textarea></div>");
         $(".post-it-orange").draggable();
       } else {
         purpleID++;
-        $("main").append("<div class='post-it-purple' id='purple-'"+purpleID+"><p><button class='minimize-post-it'>_</button> <button class='maximize-post-it'>※</button> <button class='delete-post-it-button'>X</button></p><textarea></textarea></div>");
+        $("main").append("<div class='post-it-purple' id='purple-'"+purpleID+"><p><button class='minimize-post-it'>_</button> <button class='maximize-post-it'>※</button> <button class='delete-post-it-button'>X</button></p><textarea maxlength='140'></textarea></div>");
         $(".post-it-purple").draggable();
       }
     });
