941934c2d31c43a6d3a39081b5f1e2a855927160
daviddrm52 <davidgranada2015@gmail.com>
2024-02-02 17:20:28 +0100

    more things work

--- UD6/Activity_5/js/spotify-client.js
+++ UD6/Activity_5/js/spotify-client.js
@@ -1,5 +1,5 @@
-var client_id= '';
-var client_secret = '';
+var client_id= 'be6e01e8304449ce9046c615d8a5a57d';
+var client_secret = 'c97b065446164965881b98ae75457e50';
 var access_token = '';
 
 
@@ -28,14 +28,13 @@ Spotify.prototype.getArtist = function (artist) {
       } else {
         var artistImage = '<a href="'+response.artists.items[index].external_urls.spotify+'"> <img class="artist-img" src="'+response.artists.items[index].images[1].url+'"> </img> </a>';
       }
-      $("#artists").append("<div class='artist'> <button id='artist_name'> <h2> "+ response.artists.items[index].name+" </h2> </button> <h3 class='artist_popularity'> Popularity of the artist: "+response.artists.items[index].popularity+" </h3> "+artistImage+" </div>");
+      $("#artists").append("<div class='artist'> <h2> <a href='javascript:void(0)' class='artist-name' data-id='"+response.artists.items[index].id+"'> "+ response.artists.items[index].name+" </a> </h2> <h3 class='artist_popularity'> Popularity of the artist: "+response.artists.items[index].popularity+" </h3> "+artistImage+" </div>");
     });
   });
 };
 
 //Search the albums of an artist, given the id of the artist
 Spotify.prototype.getArtistById = function (artistId) {
-
   $.ajax({
     type: "GET",
     url: this.apiUrl + 'v1/artists/' + artistId + '/albums',
@@ -44,11 +43,49 @@ Spotify.prototype.getArtistById = function (artistId) {
     },
   }).done( function(response){
     console.log(response);
+    let placeholder = "https://www.scdn.co/i/_global/open-graph-default.png";
+    $("#results").empty();
+    $("#results").append("<div id='artist-albums'> </div>");
+    $.each(response.items, function(index) {
+      if($.isEmptyObject(response.items[index].images)){
+        var albumImage = '<a href="'+response.items[index].external_urls.spotify+'"> <img class="artist-img" src="'+placeholder+'"> </img> </a>';
+      } else {
+        var albumImage = '<a href="'+response.items[index].external_urls.spotify+'"> <img class="artist-img" src="'+response.items[index].images[1].url+'"> </img> </a>';
+      }
+      $("#artist-albums").append("<div class='album'> <h2> <a href='javascript:void(0)' class='album-name' data-id='"+response.items[index].id+"'> "+ response.items[index].name+" </a> </h2> "+albumImage+" </div>");
+    });
   });
 };
 
+//Display all the songs of the album, given the album id
+Spotify.prototype.getTracksfromAlbumId = function (albumId) {
+  $.ajax({
+    type: "GET",
+    url: this.apiUrl + 'v1/albums/' + albumId + '/tracks',
+    headers: {
+      'Authorization' : 'Bearer ' + access_token
+    },
+  }).done( function(response){
+    console.log(response);
+    $("#results").empty();
+    $("#results").append("<div id='albums-tracks'> </div>");
+    $.each(response.items, function(index) {
+      $("#albums-tracks").append("<div class='track'> <h2> "+ response.items[index].name+"</h2> </div>");
+    });
+  });
+}
+
 //This fragment is the first thing that is loaded, when the $(document).ready
 $(function () {
+  $.ajax({
+    url: 'keys.json',
+    dataType: 'json',
+    success: function (data) {
+      client_id = data.client_id;
+      client_secret = data.client_secret;
+    }
+  });
+
   $.ajax({
     type: "POST",
     url: "https://accounts.spotify.com/api/token",
@@ -67,9 +104,11 @@ $(function () {
     spotify.getArtist($('#artistName').val());
   });
 
-  $('#artist-name').on('click', function () {
+  $(document).on('click', '.artist-name', function () {
     spotify.getArtistById($(this).attr("data-id"));
-    console.log("pressed");
   });
 
+  $(document).on('click', '.album-name', function () {
+    spotify.getTracksfromAlbumId($(this).attr("data-id"));
+  })
 });
