569a9cd108c6237225a9b6737e6d7646dec9e820
daviddrm52 <davidgranada2015@gmail.com>
2024-02-09 17:18:19 +0100

    moving on, now has css and more

--- UD7/Activity_1/posts-hub.js
+++ UD7/Activity_1/posts-hub.js
@@ -8,25 +8,32 @@ createApp({
       briefSummary: '',
       postContent: '',
       author: '',
-      postImage: ''
     }
   },
   methods: {
-    createPost: function(e){
-        var post = {
-            "title": this.title,
-            "briefSummary": this.briefSummary,
-            "postContent": this.postContent,
-            "author": this.author,
-            "postImage": this.postImage
-        }
-        console.log(this.postImage);
-        this.posts.push(post);
-        this.title = "";
-        this.briefSummary = "";
-        this.postContent = "";
-        this.author = "";
-        this.postImage = "";
+    publishPost: function(e){
+      var creationDate = new Date();
+      console.log(creationDate);
+
+      var post = {
+        "title": this.title,
+        "briefSummary": this.briefSummary,
+        "postContent": this.postContent,
+        "author": this.author,
+        "creationDate": creationDate
+      }
+      this.posts.push(post);
+      this.title = "";
+      this.briefSummary = "";
+      this.postContent = "";
+      this.author = "";
+      console.log(this.posts);
+    },
+    editPost: function(e){
+      //Awaiting post to edit
+    },
+    deletePost: function(e){
+      //Awaiting post to delete
     }
   }
 }).mount('#app');
