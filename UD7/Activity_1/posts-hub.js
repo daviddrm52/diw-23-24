73e8dffaff043fc93f8de439346a82c91ed41d1a
daviddrm52 <davidgranada2015@gmail.com>
2024-02-16 17:18:09 +0100

    at least edit post works...

--- UD7/Activity_1/posts-hub.js
+++ UD7/Activity_1/posts-hub.js
@@ -3,24 +3,27 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+      //posts array
       posts: [],
+      //Information inside the posts array
       id: 1,
       title: '',
       briefSummary: '',
       postContent: '',
       author: '',
       image: '',
+      postStatus: '',
       editing: false,
       url: null,
+      //Where the results of a search will be stored
+      result: []
     }
   },
   methods: {
     publishPost: function(e){
       var creationDate = new Date().toLocaleDateString("es-ES", {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric',});      
-      console.log(this.image.value);
-      var imageInput = document.getElementById("postImage");
-      this.image = "./stored_img/"+imageInput.files[0].name;
-      console.log(this.image);
+      var publicationDate = new Date().toLocaleDateString("es-ES", {year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric',});      
+      this.image = "./stored_img/"+this.$refs.postImage.files[0].name;
       var post = {
         id: this.id,
         title: this.title,
@@ -28,7 +31,9 @@ createApp({
         postContent: this.postContent,
         author: this.author,
         image: this.image,
-        creationDate: creationDate
+        postStatus: 'published',
+        creationDate: creationDate,
+        publicationDate: publicationDate
       }
       this.posts.push(post);
       this.id = this.id + 1;
@@ -37,34 +42,58 @@ createApp({
       this.postContent = "";
       this.author = "";
       this.url = null;
-      imageInput.value = "";
+      this.$refs.postImage.value = "";
       console.log(this.posts);
     },
     editPost: function(postId){
       console.log("Editing post...");
       this.editing = true;
-      console.log(postId);
-
-      const result = this.posts.find(({id}) => id === postId);
-      console.log(result);
-      console.log(result.author);
-      console.log(result.image);
-
-      // this.title = this.posts[id].title;
-      // this.briefSummary = this.posts[id].briefSummary;
-      // this.postContent = this.posts[id].postContent;
-      // this.author = this.posts[id].author;
-      // this.url = this.posts[id].image;
-
+      this.result = this.posts.find(({id}) => id === postId);
+      this.title = this.result.title;
+      this.briefSummary = this.result.briefSummary;
+      this.postContent = this.result.postContent;
+      this.author = this.result.author;
+      this.url = this.result.image;
+      this.$refs.saveEditPostButton.style.display = "block";
+      this.$refs.publishPostButton.style.display = "none";
+      this.$refs.saveDraftButton.style.display = "none";
     },
-    deletePost: function(id){
+    deletePost: function(postId){
       console.log("Deleting post...");
-      this.posts.splice(id);
+      this.result = this.posts.find(({id}) => id === postId);
+      console.log(this.result.id);
+      this.posts.splice(postId);
       console.log(this.posts);
     },
-    updatePost: function(e){
-      //Awaiting data
+    saveEditPost: function(postId){
+      this.editing = false;
+      if(this.$refs.postImage.value === ''){
+        console.log(this.result.title);
+        this.image = this.result.image;
+      } else {
+        this.image = "./stored_img/"+$refs.postImage.files[0].name;
+      }
+      var postEdit = {
+        id: this.result.id,
+        title: this.title,
+        briefSummary: this.briefSummary,
+        postContent: this.postContent,
+        author: this.author,
+        image: this.image,
+        postStatus: this.result.postStatus,
+        creationDate: this.result.creationDate,
+        publicationDate: this.result.publicationDate
+      }
+      console.log(postEdit);
+      // this.posts()
+      this.title = "";
+      this.briefSummary = "";
+      this.postContent = "";
+      this.author = "";
+      this.url = null;
+      this.$refs.postImage.value = "";
     },
+    //Will be implemented in the near future
     saveDraft: function(e){
 
     },
